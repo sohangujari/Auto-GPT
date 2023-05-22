8f074cc5e4817d15df1aa623899eaaf494b9f3b8
Sohan Gujari <103947095+sohangujari@users.noreply.github.com>
2023-05-22 20:11:38 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,81 @@
+import speech_recognition as sr
+import win32com.client
+import webbrowser
+import os
+from datetime import datetime
+import openai
+
+openai.api_key = ("API_KEY")
+speech = win32com.client.Dispatch("SAPI.SpVoice")
+now = datetime.now()
+
+def say(text):
+    speech.Speak(text)
+
+def gpt(tprompt):
+    response = openai.Completion.create(
+    engine="text-davinci-003",
+    prompt=tprompt,
+    temperature=0.7,
+    max_tokens=256,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+    )
+    say(response.choices[0].text)
+
+def takeCommand():
+    reco = sr.Recognizer()
+    with sr.Microphone() as source:
+        reco.pause_threshold = 0.8
+        audio = reco.listen(source)
+        try:
+            query = reco.recognize_google(audio, language = "en-in")
+            print(f"User: {query}")
+            return query
+        except Exception as e:
+            return "Some error occurred. Sorry from AutoGPT"
+
+say("Hello, I'm AutoGPT")
+say("How can i help you")
+
+while True:
+    print("Listening...")
+    query = str(takeCommand())
+
+    sites = [["youtube","https://www.youtube.com/"],
+            ["dribbble","https://dribbble.com/"],
+            ["gmail","https://mail.google.com"],
+            ["linkedin","https://www.linkedin.com/feed/"]]
+    for site in sites:
+        if f"open {site[0]}" in query.lower():
+            webbrowser.open(site[1])
+            say(f"Opening {site[0].capitalize()}...")
+
+    if "play song" in query.lower():
+        os.startfile("D:\Calm Down.mp3")
+        say("Playing Song...")
+
+    if "open photoshop" in query.lower():
+        os.startfile("C:\Program Files\Adobe\Adobe Photoshop CC 2019\Photoshop.exe")
+        say("Opening Photoshop...")
+
+    if "the time" in query.lower():
+        time = now.strftime("%I")
+        minutes = now.strftime("%M")
+        period = now.strftime("%p")
+        say(f"It's {time}{minutes}{period}")
+        print(f"{time}:{minutes} {period}")
+
+    if "on youtube" in query.lower():
+        text=query.lower()
+        rep_text = text.replace("watch ", "").replace(" on youtube", "")
+        link_text = rep_text.replace(" ","+")
+        website = (f"https://www.youtube.com/results?search_query={link_text}")
+        webbrowser.open(website)
+        say(f"watch {rep_text}")
+
+    gpt(tprompt=f"User: {query.lower()}\n AutoGPT: ")
+    
+    if (query.lower() == "ok bye"):
+        break
